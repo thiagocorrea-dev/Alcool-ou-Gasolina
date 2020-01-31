aebc7c445ee1a214dbb8f5a7622e04f7b2b52c78
Thiago Corrêa <57046155+tgcbotelho@users.noreply.github.com>
2020-01-31 12:47:04 -0300

    Gasolina ou Álcool - Ajuste do main

---
+++ alcool_gasolina/lib/bloc/bloc.mask.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class mascara {
+  var _gasCtrl = new MoneyMaskedTextController();
+}
--- alcool_gasolina/lib/main.dart
+++ alcool_gasolina/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:alcool_gasolina/bloc/bloc.mask.dart';
 import 'package:alcool_gasolina/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,13 +20,67 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: ListView(
         children: <Widget>[
-         Logo(),
+          Logo(),
+          Row(
+            children: <Widget>[
+              Container(
+                width: 100,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "Gasolina",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 35,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: TextFormField(
+                  controller: _gasCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text("Álcool"),
+              Expanded(
+                child: TextFormField(
+                  controller: _gasCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
